41484d3d63cf8c860f81f06db0a624ade57a0936
Lautaro1387 <lautaro12387@gmail.com>
2022-08-23 10:02:52 -0700

    Changing the bubbles file and fixing it

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,23 +6,21 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	int j, temp, sort;
+	size_t i, j, temp;
 
-	while (1)
+	if (array == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
 	{
-		sort = 0;
-		for (j = 1; (size_t)j < size; j++)
+		for (j = 0; j < size - 1; j++)
 		{
-			if (array[j - 1] > array[j])
+			if (array[j] > array[j + 1])
 			{
-				sort = 1;
 				temp = array[j];
-				array[j] = array[j - 1];
-				array[j - 1] = temp;
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
 				print_array(array, size);
 			}
 		}
-	if (sort == 0)
-		break;
-	}
 }
