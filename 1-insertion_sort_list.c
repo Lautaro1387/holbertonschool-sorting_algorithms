786da5ac8d84026fcce9bab7f08ddb8fa6020fee
ayamorcito <izquierdoz.g@gmail.com>
2022-08-24 13:56:23 -0700

    1 ready for check

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,47 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorts an array of int using the Sel-Sort algorithm
+ * @list: list to be sorted
+ *
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *now, *nowcp;
+	int tmp = 0;
+
+	now = *list;
+
+	if (list == NULL || *list == NULL)
+		return;
+
+	while (now)
+	{
+		nowcp = now->next;
+
+		while (now->prev && now->n < now->prev->n)
+		{
+			now->prev->next = now->next;
+
+			if (now->next)
+			{
+				now->next->prev = now->prev;
+				now->next = now->prev;
+				now->prev = now->prev->prev;
+			}
+
+			if (now->prev)
+			{
+				now->prev->next = now;
+				now->next->prev = now;
+			}
+
+			if (!now->prev)
+			{
+				*list = now;
+				print_list(*list);
+			}
+		}
+		now = nowcp;
+	}
+}
