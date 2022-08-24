75941284f3f300d734c5379dad074da718e55fc8
Lautaro1387 <lautaro12387@gmail.com>
2022-08-24 07:10:21 -0700

    Fix

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -6,8 +6,8 @@
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j;
-	int temp, min, order;
+	size_t i, j, min;
+	int temp;
 
 
 	if (!array)
@@ -15,18 +15,20 @@ void selection_sort(int *array, size_t size)
 
 	for (i = 0; i < size - 1; i++)
 	{
-		min = array[i];
+		min = i;
 		for (j = i + 1; j < size; j++)
 		{
-			if (array[j] < min)
+			if (array[j] < array[min])
 			{
-				min = array[j];
-				order = j;
+				min = j;
 			}
 		}
+		if (min != i)
+		{
 			temp = array[i];
-			array[i] = min;
-			array[order] = temp;
+			array[i] = array[min];
+			array[min] = temp;
 			print_array(array, size);
+		}
 	}
 }
